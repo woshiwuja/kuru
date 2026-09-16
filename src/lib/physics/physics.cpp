321ce124b31b11592a0e3e9760c3ff2be70a81e6
woshiwuja <gabusif@outlook.it>
2026-09-16 17:08:50 +0200

    Create physics.cpp

---
+++ src/lib/physics/physics.cpp
@@ -0,0 +1,34 @@
+#include "physics.hpp"
+#include "../core/core.hpp"
+#include <iostream>
+namespace KR {
+    void PhysicsManager::init() {
+      std::cerr << "[debug] PhysicsManager::init: new Factory\n" << std::flush;
+      JPH::Factory::sInstance = new JPH::Factory();
+      std::cerr << "[debug] PhysicsManager::init: RegisterTypes\n" << std::flush;
+      JPH::RegisterTypes();
+
+      std::cerr << "[debug] PhysicsManager::init: MapObjectToBroadPhaseLayer\n" << std::flush;
+      bpLayers.MapObjectToBroadPhaseLayer(NON_MOVING, BD_NON_MOVING);
+      bpLayers.MapObjectToBroadPhaseLayer(MOVING, BD_MOVING);
+      std::cerr << "[debug] PhysicsManager::init: EnableCollision\n" << std::flush;
+      objectPairs.EnableCollision(MOVING, NON_MOVING);
+      objectPairs.EnableCollision(MOVING, MOVING);
+      std::cerr << "[debug] PhysicsManager::init: objVsBpLayers.emplace\n" << std::flush;
+      objVsBpLayers.emplace(bpLayers, BD_NUM_LAYERS, objectPairs,
+                            NUM_LAYERS);
+      std::cerr << "[debug] PhysicsManager::init: system.Init\n" << std::flush;
+      system.Init(maxBodies, numBodyMutexes, maxBodyPairs, maxContactConstraints,
+                  bpLayers, *objVsBpLayers, objectPairs);
+      std::cerr << "[debug] PhysicsManager::init: done\n" << std::flush;
+    }
+    void PhysicsManager::update() {
+          auto dt = Core::get()->deltaTime;
+          system.Update(dt, 1, &tempAllocator, &jobSystem);
+        }
+    void PhysicsManager::stop(){
+            JPH::UnregisterTypes();
+            delete JPH::Factory::sInstance;
+            JPH::Factory::sInstance = nullptr;
+    }
+}
